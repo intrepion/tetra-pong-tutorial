c423f31925fac3624e7f43b6fb1200a6d1c3893c
Oliver Forral <intrepion@gmail.com>
2021-05-14 23:47:05 -0700

    Reacting to Input

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -5,15 +5,23 @@ use tetra::{Context, ContextBuilder, State};
 
 const WINDOW_WIDTH: f32 = 640.0;
 const WINDOW_HEIGHT: f32 = 480.0;
+const PADDLE_SPEED: f32 = 8.0;
 
 struct GameState {
     paddle_texture: Texture,
+    paddle_position: Vec2<f32>,
 }
 
 impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
         let paddle_texture = Texture::new(ctx, "./resources/player1.png")?;
-        Ok(GameState { paddle_texture })
+        let paddle_position =
+            Vec2::new(16.0, (WINDOW_HEIGHT - paddle_texture.height() as f32) / 2.0);
+
+        Ok(GameState {
+            paddle_texture,
+            paddle_position,
+        })
     }
 }
 
@@ -21,7 +29,19 @@ impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
     
-        self.paddle_texture.draw(ctx, Vec2::new(16.0, 16.0));
+        self.paddle_texture.draw(ctx, self.paddle_position);
+    
+        Ok(())
+    }
+
+    fn update(&mut self, ctx: &mut Context) -> tetra::Result {
+        if input::is_key_down(ctx, Key::W) {
+            self.paddle_position.y -= PADDLE_SPEED;
+        }
+    
+        if input::is_key_down(ctx, Key::S) {
+            self.paddle_position.y += PADDLE_SPEED;
+        }
     
         Ok(())
     }
