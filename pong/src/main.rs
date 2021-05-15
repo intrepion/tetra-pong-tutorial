b9f205cc3e283ddbdfb7afec65f62983efb8068b
Oliver Forral <intrepion@gmail.com>
2021-05-14 23:14:34 -0700

    Running the Game Loop

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -5,8 +5,9 @@ struct GameState {}
 
 impl State for GameState {}
 
-fn main() {
+fn main() -> tetra::Result {
     ContextBuilder::new("Pong", 640, 480)
         .quit_on_escape(true)
-        .build();
+        .build()?
+        .run(|_| Ok(GameState {}))
 }
