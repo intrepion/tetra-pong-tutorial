bca9dbfed0a76900429bb0fb9470f88788086106
Oliver Forral <intrepion@gmail.com>
2021-05-14 23:13:54 -0700

    Defining Some State

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -1,6 +1,10 @@
 use tetra::graphics::{self, Color};
 use tetra::{Context, ContextBuilder, State};
 
+struct GameState {}
+
+impl State for GameState {}
+
 fn main() {
     ContextBuilder::new("Pong", 640, 480)
         .quit_on_escape(true)
